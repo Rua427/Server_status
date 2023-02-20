86fc628c05e2947e0e1f34b217772ea8308fe054
Rua <70061403+Rua427@users.noreply.github.com>
2023-02-20 18:27:21 +0900

    Create serverChart.tsx

---
+++ src/Component/Content/RightSide/server_chart/serverChart.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ChartTemplate from './chartTemplate'
+import './serverChart.css'
+const ServerChart = () => {
+  return (
+    <div className='serverChart'>
+      <ChartTemplate/>
+      <ChartTemplate/>
+      <ChartTemplate/>
+      <ChartTemplate/>
+    </div>
+  )
+}
+
+export default ServerChart
