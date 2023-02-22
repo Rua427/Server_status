b2019fec4ea6fbc645b73cbf0b3decebfe483616
Rua <70061403+Rua427@users.noreply.github.com>
2023-02-22 18:11:10 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Component/Header/header';
 import Main from './Component/Content/main';
 import Navigation from './Component/Navigation/NaviagationTemplate/Navigation';
 
