0f4b1bec1ed31b0bd146ff3ab4a494da2e6d4afd
Rua <70061403+Rua427@users.noreply.github.com>
2023-02-22 18:06:39 +0900

    Update chartTemplate.tsx

--- src/Component/Content/RightSide/server_chart/chartTemplate.tsx
+++ src/Component/Content/RightSide/server_chart/chartTemplate.tsx
@@ -7,70 +7,70 @@ import {
     Tooltip,
     Legend,
 } from 'chart.js';
-import { PolarArea } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
 import  styled  from 'styled-components';
 import './chartTemplate.css'
 import { ChartJSOrUndefined } from 'react-chartjs-2/dist/types';
 
-ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
+ChartJS.register(
+  RadialLinearScale,
+  ArcElement,
+  Tooltip,
+  Legend
+);
 
 
 export const options={
-    responsive:false,
-    plugins:{
-        legend:{
-            position:'right' as const,
-            align:'center' as const,
-            fullsize:true,
-        },
-        title:{
-            display:true,
-            text:'DashBoard',
-        }
-
-    }
+  maintainAspectRatio:false,
+  responsive:true,
+  plugins:{
+      legend:{
+        position:'right' as const,
+        align:'center' as const,
+        fullsize:true,
+      },
+      subtitle: {
+        display: true,
+        text: 'Custom Chart Subtitle'
+      },
+  },
+  layout:{
+    padding:20
+  }
 }
 export const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Good', 'Warning', 'Bad', 'Disconnect'],
     datasets: [
       {
         label: '# of Count',
-        data: [Math.random() * 20, Math.random() * 20,Math.random() * 20,Math.random() * 20,Math.random() * 20,Math.random() * 20],
+        data: [Math.random() * 20, Math.random() * 20,Math.random() * 20,Math.random() * 20],
         backgroundColor: [
           'rgba(255, 99, 132, 0.5)',
           'rgba(54, 162, 235, 0.5)',
           'rgba(255, 206, 86, 0.5)',
           'rgba(75, 192, 192, 0.5)',
-          'rgba(153, 102, 255, 0.5)',
-          'rgba(255, 159, 64, 0.5)',
         ],
         borderWidth: 2,
-        borderColor: 'rgba(0,0,0,0.5)',
+        borderColor: 'rgba(120,120,120,1)',
       },
     ],
 };
 const Container = styled.div`
-  width: 30vw;
-  max-width: 100vw;
+  height:220px;
 `;
 
-const ChartTemplate = () => {
 
-    const polarRef = useRef<ChartJSOrUndefined>(null);
+interface Props{
+  serverName: string,
 
+}
 
-    useEffect(() => {
-        const chart = polarRef.current;
-
-        if(chart){
-            chart.width= 500;
-            chart.height= 200;
-        }
-    },[]);
+const ChartTemplate = (props :Props )  => {
   return (
     <div className='chartTemplate'>
+        <h2>{props.serverName} Status</h2>
         <Container>
-            <PolarArea ref={polarRef} data={data} options={options} />
+          <Doughnut data={data} options={options}  style={{position:"relative"}}/>
         </Container>
     </div>
   )
