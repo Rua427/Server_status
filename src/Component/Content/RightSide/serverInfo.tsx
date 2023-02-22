f9bdc420a1b73b5cdb72b7e41485404598d78964
Rua <70061403+Rua427@users.noreply.github.com>
2023-02-22 18:10:25 +0900

    Update and rename ServerInfo.tsx to serverInfo.tsx

--- src/Component/Content/RightSide/serverInfo.tsx
+++ src/Component/Content/RightSide/serverInfo.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Server from './server';
+import {Tooltip} from 'react-tooltip'
 import './serverInfo.css'
+import 'react-tooltip/dist/react-tooltip.css';
 const ServerInfo = () => {
     let s: any = [];
 
@@ -8,23 +10,28 @@ const ServerInfo = () => {
         for(let x = 0; x < 6; x++){
 
             const r = Math.floor(Math.random() * 100);
-
+            let num = 0;
             if(r >= 99){
-              s = s.concat(<Server key={`${x},${y}`} status={2}/>)
+              num = 2;
             }
+
             else if(r >= 92){
-              s = s.concat(<Server key={`${x},${y}`} status={1}/>)
+              num = 1
             }
             else{
-              s = s.concat(<Server key={`${x},${y}`} status={0}/>)
+              num = 0;
             }
+
+            s = s.concat(<a data-tooltip-id="my-tooltip"><Server key={`${x},${y}`} status={num}/></a>)
+
         }
     }
-
 
   return (
     <div className='serverInfo'>
         {s}
+        <Tooltip id="my-tooltip"
+        content='this is a server status area'/>
     </div>
   )
 }
