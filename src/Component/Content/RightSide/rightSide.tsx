da9178c3d667641804d8e25cfb1fe4a9648cd385
Rua <70061403+Rua427@users.noreply.github.com>
2023-04-24 20:24:50 +0900

    Update rightSide.tsx

--- src/Component/Content/RightSide/rightSide.tsx
+++ src/Component/Content/RightSide/rightSide.tsx
@@ -8,42 +8,26 @@ import ChartServers from './server_chart/chartServers'
 import SideSelection from '../../../Context'
 import axios from 'axios';
 import { json } from 'stream/consumers'
-
+import type {ServerList } from '../../../DBType' 
+import { list } from '../../../DBType' 
 const RightSide = () => {
-  let servers: {[key: string]: number} = { };
-  
+
   // data 배열 인덱싱으로 mysql table내 column을 이용
-  const [data, setDatas] = useState<any>(null);
+  const [serverlist, setServerList] = useState<ServerList[]>(list);
   const [error, setError] = useState<any>(null);
+  
   const getDatas = async () =>{
-
-    servers["vDU"] = 5;
-    servers["USM"] = 50;
-    servers["LSM"] = 24;
-    servers["Database"] = 10;
-    servers["ETC"] = 10;
-
-    try{
       setError("");
-      setDatas(null);
 
       // 해당 response로 서버 개수 
-      const response = await axios.get(
-        'api/data'
+      const response = await axios.get<ServerList[]>(
+        'http://10.240.60.92:3301/api/data'
       );
-
-      // 서버 테스트 결과 
-
-
-      setDatas(response.data);
-    }
-    catch(e){
-    }
+      return response;
   }
   useEffect(() => {
-    getDatas();
-  })
-
+    getDatas().then(res => setServerList(res.data));
+  }, [])
 
 
   return (
@@ -54,7 +38,7 @@ const RightSide = () => {
           <div className={`${value === "Dashboard" ? "" : "disable"}`}><ChartDashboard/></div>
           <div className={`${value !== "Dashboard" ? value : "disable"}`}><ChartServers chartCategoryName={value}/></div>
           {/* 여기에 각 카테리별 서버 개수 집어넣어야 함.*/}
-          <div><ServerInfo server={value} serverCount={servers}/></div>
+          <div><ServerInfo server={value} serverCount={serverlist}/></div>
           {/* <ServerStatus/> */}
         </div>  
       )}
