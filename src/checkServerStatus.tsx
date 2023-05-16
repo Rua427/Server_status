e1c58939b45ff668744587747091cd37e3215d58
Rua <70061403+Rua427@users.noreply.github.com>
2023-05-16 19:20:09 +0900

    Update checkServerStatus.tsx

--- src/checkServerStatus.tsx
+++ src/checkServerStatus.tsx
@@ -1,10 +1,15 @@
-import {ServerResult} from './DBType'
+import {serverResult, ServerResult} from './DBType'
 
 export type CheckStatus = {
     result: number,
     reason: string[],
 }
-
+export type StatusCount = {
+    good: number,
+    warning: number,
+    bad: number,
+    disconnect: number,
+}
 
 export const checkServerStatus = (serverStatus: ServerResult) => {
     let check: number = 0;
@@ -13,18 +18,99 @@ export const checkServerStatus = (serverStatus: ServerResult) => {
         reason: [],
     };
 
-    if(serverStatus.CPU_Temp > 0.9) { check < 2 ? 2 : check; checking.reason.push("CPU Temp is too high")}
-    if(serverStatus.CPU_Usage > 0.9) { check < 2 ? 2 : check; checking.reason.push("CPU Usage is too a lot")}
-    if(serverStatus.Disk_Usage > 0.9) { check < 2 ? 2 : check; checking.reason.push("Disk Usage is too a lot")}
-    if(serverStatus.Memory_Usage > 0.9) { check < 2 ? 2 : check; checking.reason.push("Memory Usage is too a lot")}
-    if(serverStatus.GPS === "bad") { check < 2 ? 2 : check; checking.reason.push("vDU GPS stats is wrong") }
-    if(serverStatus.VNFD === "bad") { check < 2 ? 2 : check; checking.reason.push("vDU VNFD state is wrong")}
-    if(serverStatus.CPU_Temp > 0.8 && serverStatus.CPU_Temp <= 0.9) { check < 1 ? 1 : check; checking.reason.push("CPU Temp is high")}
-    if(serverStatus.CPU_Usage > 0.8 && serverStatus.CPU_Usage <= 0.9) { check < 1 ? 1 : check; checking.reason.push("CPU Usage is a lot")}
-    if(serverStatus.Disk_Usage > 0.8 && serverStatus.Disk_Usage <= 0.9) { check < 1 ? 1 : check; checking.reason.push("Disk Usage is a lot")}
-    if(serverStatus.Memory_Usage > 0.8 && serverStatus.Memory_Usage <= 0.9) { check < 1 ? 1 : check; checking.reason.push("Memory Usage is a lot")}
+    if(serverStatus.CPU_Temp > 90) { check = check < 2 ? 2 : check; checking.reason.push("CPU Temp is too high"); }
+    if(serverStatus.CPU_Usage > 0.9) { check = check < 2 ? 2 : check; checking.reason.push("CPU Usage is too a lot"); }
+    if(serverStatus.Disk_Usage > 0.9) { check = check < 2 ? 2 : check; checking.reason.push("Disk Usage is too a lot"); }
+    if(serverStatus.Memory_Usage > 0.9) { check = check < 2 ? 2 : check; checking.reason.push("Memory Usage is too a lot"); }
+    if(serverStatus.GPS === "bad") { check = check < 2 ? 2 : check; checking.reason.push("vDU GPS stats is wrong"); }
+    if(serverStatus.VNFD === "bad") { check = check < 2 ? 2 : check; checking.reason.push("vDU VNFD state is wrong"); }
+    if(serverStatus.CPU_Temp > 80 && serverStatus.CPU_Temp <= 90) { check = check < 1 ? 1 : check; checking.reason.push("CPU Temp is high"); }
+    if(serverStatus.CPU_Usage > 0.8 && serverStatus.CPU_Usage <= 0.9) { check = check < 1 ? 1 : check; checking.reason.push("CPU Usage is a lot"); }
+    if(serverStatus.Disk_Usage > 0.8 && serverStatus.Disk_Usage <= 0.9) { check = check < 1 ? 1 : check; checking.reason.push("Disk Usage is a lot"); }
+    if(serverStatus.Memory_Usage > 0.8 && serverStatus.Memory_Usage <= 0.9) { check = check < 1 ? 1 : check; checking.reason.push("Memory Usage is a lot"); }
 
     checking.result = check;
+
+    return checking;
+}
+export const checkEachServerStatus = (serverStatus: ServerResult[], category: string) => {
+    let check: number = 0;
+    let checking: StatusCount = {
+        good: 0,
+        warning: 0,
+        bad: 0,
+        disconnect: 0,
+    };
+    serverStatus.map((res) => {
+        check = 0;
+        if(category === res.server_category){
+            if(res.CPU_Temp > 90) { check = check < 2 ? 2 : check; }
+            if(res.CPU_Usage > 0.9) { check = check < 2 ? 2 : check; }
+            if(res.Disk_Usage > 0.9) { check = check < 2 ? 2 : check;  }
+            if(res.Memory_Usage > 0.9) { check = check < 2 ? 2 : check;  }
+            if(res.GPS === "bad") { check = check < 2 ? 2 : check; }
+            if(res.VNFD === "bad") { check = check < 2 ? 2 : check;  }
+            if(res.CPU_Temp > 80 && res.CPU_Temp <= 90) { check = check < 1 ? 1 : check; }
+            if(res.CPU_Usage > 0.8 && res.CPU_Usage <= 0.9) { check = check < 1 ? 1 : check; }
+            if(res.Disk_Usage > 0.8 && res.Disk_Usage <= 0.9) { check = check < 1 ? 1 : check; }
+            if(res.Memory_Usage > 0.8 && res.Memory_Usage <= 0.9) { check = check < 1 ? 1 : check; }
+            
+            if(check == 2){
+                checking.bad ++;
+            }
+            else if(check == 1){
+                checking.warning++;
+            }
+            else{
+                checking.good++;
+            }
+        }
+    })
+
+    return checking;
+}
+
+
+export const checkServerEachItemStatus = (serverStatus: ServerResult[], category: string, item: string) => {
+    let check: number = 0;
+    let checking: StatusCount = {
+        good: 0,
+        warning: 0,
+        bad: 0,
+        disconnect: 0,
+    };
+    serverStatus.map((res) => {
+        if(category === res.server_category){
+            if(item === "CPU_Temp") {
+                if(res.CPU_Temp > 90) { check = check < 2 ? 2 : check; checking.bad++}
+                else if(res.CPU_Temp > 80 && res.CPU_Temp <= 90) { check = check < 1 ? 1 : check; checking.warning++; }
+                else{checking.good++;}
+            }
+            else if(item === "CPU_Usage") {
+                if(res.CPU_Usage > 0.9) { check = check < 2 ? 2 : check; checking.bad++}
+                else if(res.CPU_Usage > 0.8 && res.CPU_Usage <= 0.9) { check = check < 1 ? 1 : check; checking.warning++; }
+                else{checking.good++;}
+            }
+            else if(item === "Disk_Usage") {
+                if(res.Disk_Usage > 0.9) { check = check < 2 ? 2 : check; checking.bad++}
+                else if(res.Disk_Usage > 0.8 && res.Disk_Usage <= 0.9) { check = check < 1 ? 1 : check; checking.warning++; }
+                else{checking.good++;}
+            }
+            else if(item === "Memory_Usage") {
+                if(res.Memory_Usage > 0.9) { check = check < 2 ? 2 : check; checking.bad++}
+                else if(res.Memory_Usage > 0.8 && res.Memory_Usage <= 0.9) { check = check < 1 ? 1 : check; checking.warning++; }
+                else{checking.good++;}
+            }
+            else if(item === "GPS"){
+                if(res.GPS === "bad") { check = check < 2 ? 2 : check; checking.bad++; }
+                else{checking.good++;}
+            }
+            else if(item === "VNFD" ){
+                if(res.VNFD === "bad") { check = check < 2 ? 2 : check; checking.bad++; }
+                else{checking.good++;}
+            }
+        }
+    })
 
     return checking;
 }
