5030110d78ce635cca2c19c26c8774b0656768ea
Rua <70061403+Rua427@users.noreply.github.com>
2023-05-16 19:25:37 +0900

    Update chartDashboard.tsx

--- src/Component/Content/RightSide/server_chart/Dashboard/chartDashboard.tsx
+++ src/Component/Content/RightSide/server_chart/Dashboard/chartDashboard.tsx
@@ -1,38 +1,54 @@
 import React from 'react'
 import ChartTemplate from '../chartTemplate'
 import './chartDashboard.css'
+import axios from 'axios';
+import { ServerResult } from '../../../../../DBType';
+import { list, serverResult } from '../../../../../DBType' 
+import { checkEachServerStatus } from '../../../../../checkServerStatus' 
 
 interface props{
-  
+  enable: string
 }
 // 여기서 각 서버별 상태 개수를 api를 통해 가져온다.
 const ChartDashboard = () => {
+  const [error, setError] = useState<any>(null);
+  const [testResult, setTestResult] = useState<ServerResult[]>(serverResult)
+
+  const getResult = async () => {
+    setError("");
+
+    // 전체 결과 가져옴 (최근 결과 값)
+    const response = await axios.get<ServerResult[]>(
+      'http://10.240.60.92:3301/api/server_info_result'
+    );
+
+    return response;
+  }
+
+useEffect(() => {
+  getResult().then(res => setTestResult(res.data));
+  //getResult().then(res => setTestResult(res.data));
+}, [props.enable])
+
+
+  const createChartData = (name: string) => {
+    
+    const data = props.enable !== "Dashboard" ? checkEachServerStatus(serverResult, name) : checkEachServerStatus(testResult, name);
+
+    return (<ChartTemplate 
+                good =          {data.good}
+                warning =       {data.warning}
+                bad =           {data.bad}
+                disconnect =    {data.disconnect}
+                statusName={name}/>)
+  }
+
   return (
     <div className='chartDashboard'>
-      <ChartTemplate 
-                              good =          {Math.floor(Math.random() * 100)}
-                              warning =       {Math.floor(Math.random() * 100)}
-                              bad =           {Math.floor(Math.random() * 100)}
-                              disconnect =    {Math.floor(Math.random() * 100)}
-      statusName="vDU"/>
-      <ChartTemplate 
-                              good =          {Math.floor(Math.random() * 100)}
-                              warning =       {Math.floor(Math.random() * 100)}
-                              bad =           {Math.floor(Math.random() * 100)}
-                              disconnect =    {Math.floor(Math.random() * 100)}
-                              statusName="Database"/>
-      <ChartTemplate 
-                              good =          {Math.floor(Math.random() * 100)}
-                              warning =       {Math.floor(Math.random() * 100)}
-                              bad =           {Math.floor(Math.random() * 100)}
-                              disconnect =    {Math.floor(Math.random() * 100)}
-                              statusName="USM"/>
-      <ChartTemplate 
-                              good =          {Math.floor(Math.random() * 100)}
-                              warning =       {Math.floor(Math.random() * 100)}
-                              bad =           {Math.floor(Math.random() * 100)}
-                              disconnect =    {Math.floor(Math.random() * 100)}
-                              statusName="LSM"/>
+            {createChartData("vDU")}
+            {createChartData("DataBase")}
+            {createChartData("USM")}
+            {createChartData("LSM")}
     </div>
   )
 }
