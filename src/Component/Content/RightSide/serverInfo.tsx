c88d131960624c80aaf144ca1d44748d1253f58b
Rua <70061403+Rua427@users.noreply.github.com>
2023-05-16 19:21:05 +0900

    Update serverInfo.tsx

--- src/Component/Content/RightSide/serverInfo.tsx
+++ src/Component/Content/RightSide/serverInfo.tsx
@@ -4,50 +4,76 @@ import {Tooltip} from 'react-tooltip'
 import './serverInfo.css'
 import 'react-tooltip/dist/react-tooltip.css';
 import type {ServerList, ServerResult } from '../../../DBType' 
-import type { CheckStatus } from '../../../checkServerStatus' 
 import { checkServerStatus } from '../../../checkServerStatus' 
+import axios from 'axios';
+import { list, serverResult } from '../../../DBType' 
 
-interface Props{
-  serverCount: ServerResult[]; 
+interface ServerData{
   server : string;
+
 }
 
-
-
 // 여기에서 서버 정보 결과를 넣어준다.
-const ServerInfo = (props: Props) => {
-    let s: any = [];
-    const [serverData, setServer] = useState<Props>();
+const ServerInfo = (props: ServerData ) => {
+    let s: any;
+    const [testResult, setTestResult] = useState<ServerResult[]>(serverResult)
 
+    const [error, setError] = useState<any>(null);
+
+    const getResult = async (category: string) => {
+      setError("");
+  
+      if(category === "Dashboard"){
+        // 전체 결과 가져옴 (최근 결과 값)
+        const response = await axios.get<ServerResult[]>(
+          'http://10.240.60.92:3301/api/server_info_result'
+        );
+        return response;
+
+      }
+      else{
+        // 특정 항목 결과 가져옴 (최근 결과 값)
+        const response = await axios.get<ServerResult[]>(
+          `http://10.240.60.92:3301/api/server_info_result/${category}`
+        );
+        return response;
+      }
+    }
+  
     useEffect(() => {
-      setServer(props);
-      CreateServerPoint();
-    }, [])
-      
+      getResult(props.server).then(res => setTestResult(res.data));
+      //getResult().then(res => setTestResult(res.data));
+    }, [props.server])
+  
+
+    // useEffect(() => {
+    //   setServer({server: props.server, serverCount: props.serverCount});
+    // }, [serverData])
+
     const CreateServerPoint = () => {
       // 데이터 없으면 아무것도 생성 안함
-      if(serverData === undefined){ return false; }
+      if(testResult === undefined){ return false; }
+      if(testResult.length <= 1) { return false; }
       
       // 카테 고리 선택에 따른 표시 변경
-      serverData.serverCount.map((res) => {
-        if(res.server_category === props.server || props.server === "Dashboard"){
-          const status: CheckStatus = checkServerStatus(res);
-          s = s.concat(<a data-tooltip-id="my-tooltip"
-                          data-tooltip-html={ `IP : ${res.IP}<br/>` + 
-                                              `Name : ${res.server_name}<br/>` + 
-                                              `Category : ${res.server_category}<br/>` +
-                                              `Status : ${status.reason.length === 0 ? "GOOD" : status.reason.map((text) => {
-                                                return text + "<br/>";
-                                              })}` } >
-          <Server key={`${res.IP},${res.server_name}`} status={status.result}/>
-         </a>)
-        }
+      return testResult.map((res) => {
+          const status = checkServerStatus(res);
+          return (<a key={`${res.IP},${res.server_name}`}
+                    data-tooltip-id="my-tooltip"
+                    data-tooltip-html={ `IP : ${res.IP}<br/>` + 
+                                        `Name : ${res.server_name}<br/>` + 
+                                        `Category : ${res.server_category}<br/>` +
+                                        `Status : ${status.reason.length === 0 ? "GOOD" : status.reason.map((text) => {
+                                                                                            return text + "<br/>";
+                                                                                          })}` 
+                                      } >
+                    <Server status={status.result}/></a>)
       })
     }
 
   return (
     <div className='serverInfo'>
-        {s}
+        {CreateServerPoint()}
         <Tooltip id="my-tooltip"/>
     </div>
   )
