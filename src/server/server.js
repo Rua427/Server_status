cc7bf8a207df891aae15c2f6271cefd824669ca3
Rua <70061403+Rua427@users.noreply.github.com>
2023-05-26 18:32:18 +0900

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -19,11 +19,15 @@ app.use(cors({
 
 app.use(express.urlencoded({extended:true}))
 
+app.use(express.static(path.join(__dirname, '../build')));
+
+
 db.connect(function(err){
     if(err) throw err;
 
     console.log("Connected!");
 })
+
 
 // develop API
 // 서버 정보 파싱
@@ -75,7 +79,6 @@ app.get("/api/server_info_result/:category/", (req, res) => {
 app.get("/api/server_info_result", (req, res) => {
 
     res.header("Access-Control-Allow-Origin", "*");
-    console.log("get data");
     const sqlQuery =    "select a.*, b.server_name from server_status.server_status_result a " +
                         "left join server_status.server_info b on (a.IP = b.IP ) " +
                         "where a.Week = (SELECT MAX(Week) from server_status.server_status_result) " + 
@@ -103,11 +106,30 @@ app.get("/api/server_info_result/:IP", (req, res) => {
     })
 })
 
+// 가장최근에 업데이트 한 시간 가져옴
+app.get("/api/LastUpdate", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    const sqlQuery =    "select MAX(server_status_result.Update_Time) as t from server_status.server_status_result;"
+
+    db.query(sqlQuery, (err, result, fields) =>{
+        if(err !== null)
+            console.log(err);
+        res.send(result);
+    })
+})
+
 app.get("/data", (req, res) => {
     res.send('GET request to the homepage');
 })
 
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
 })
+
 
