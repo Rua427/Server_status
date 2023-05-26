4b6839c8be29e380ce065a30f1aa7d7de9c47074
Rua <70061403+Rua427@users.noreply.github.com>
2023-05-26 18:33:19 +0900

    Update Navigation.tsx

--- src/Component/Navigation/NaviagationTemplate/Navigation.tsx
+++ src/Component/Navigation/NaviagationTemplate/Navigation.tsx
@@ -11,6 +11,8 @@ import {HiServer} from 'react-icons/hi2';
 import { FiChevronLeft } from 'react-icons/fi';
 import  SideSelection  from '../../../Context';
 import RightSide from '../../Content/RightSide/rightSide';
+import axios from 'axios';
+import { LastUpdate, lastUpdate } from '../../../DBType' 
 
 interface Props{
   setItem: Function;
@@ -20,28 +22,40 @@ interface Props{
 
 const Navigation = (props: Props) => {
   const [selectedItem, setItem] = useState<string>("Dashboard");
+  const [lastTime, setLastTime] = useState<LastUpdate[]>(lastUpdate);
 
   useEffect(()=> {
+    getLastUpdate().then(res => setLastTime(res.data));
     props.setItem(selectedItem);
-  },[selectedItem])
+  }, [selectedItem])
 
+  const getLastUpdate = async () =>{
+    const response = await axios.get<LastUpdate[]>(
+      'http://10.240.60.92:3301/api/LastUpdate'
+    );
+
+    return response;
+  }
   return (
       <div className='navigation'>
-        <div className="menu">
+        {/* <div className="menu">
           <FiChevronLeft className='menu-icon'/>
-        </div>
+        </div> */}
         <header>
           <div className="profile">
             <SiSamsung/>
             <span>Global Manufacture Support</span>
           </div>
         </header>
+        <div className='categoryList'>
           <div onClick={() => {setItem("Dashboard")}}><Nav Icon={TbDashboard} title={"Dashboard"} isSet={selectedItem}/></div>
-          <div onClick={() => {setItem("Database")}}><Nav Icon={AiOutlineDatabase} title={"Database"} isSet={selectedItem}/></div>
+          <div onClick={() => {setItem("DataBase")}}><Nav Icon={AiOutlineDatabase} title={"DataBase"} isSet={selectedItem}/></div>
           <div onClick={() => {setItem("USM")}}><Nav Icon={HiServerStack} title={"USM"} isSet={selectedItem}/></div>
           <div onClick={() => {setItem("LSM")}}><Nav Icon={HiOutlineServerStack} title={"LSM"} isSet={selectedItem}/></div>
           <div onClick={() => {setItem("vDU")}}><Nav Icon={HiServer} title={"vDU"} isSet={selectedItem}/></div>
           <div onClick={() => {setItem("ETC")}}><Nav Icon={HiDotsHorizontal} title={"ETC"} isSet={selectedItem}/></div>
+        </div>
+        <p>{"Last Update : " + lastTime[0].t}</p> 
       </div>
   )
 }
